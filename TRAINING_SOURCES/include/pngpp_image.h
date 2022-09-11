4a93cfc2bfc7b24a34107c5145abd47d90cecb0b
hai <haidt2>
2022-09-11 11:32:21 +0700

    ok

--- TRAINING_SOURCES/include/pngpp_image.h
+++ TRAINING_SOURCES/include/pngpp_image.h
@@ -1,18 +1,17 @@
+
 #include <png++/png.hpp>
 #include <png++/image.hpp>
-#include <png++/rgb_pixel.hpp>
+#include "png++/rgb_pixel.hpp"
 #include "png.hpp"
 #include <iostream>
-
 class PngppImage
 {
 private:
     /* data */
-    uint32_t w, h;
+    std::uint32_t w, h;
 public:
-   bool OpenImage(std::string);
-    uint32_t GetW();
-    uint32_t GetH();
+   bool OpenImage(std::string path);
+    std::int GetW();
+    std::uint32_t GetH();
    void ShowImageInfo(unsigned w,unsigned h);
 };
-
--- TRAINING_SOURCES/src/cmake_images/main.cpp
+++ TRAINING_SOURCES/src/cmake_images/main.cpp
@@ -1,10 +1,13 @@
 // TODO
 
 #include "lodepng_image.h"
+#include "pngpp_image.h"
+
 
 int main(int argc, char const *argv[])
 {
     LodepngImage lode;
+    
     std::string file_path = "/home/dell/Documents/ss1/session_01_src/TRAINING_SOURCES/data/linux_test.png";
    
 
@@ -15,6 +18,12 @@ int main(int argc, char const *argv[])
         std::cout << "Test of LodepngImage is successful" <<std::endl;  
     }
     else std::cout << "Test of LodepngImage is failed" <<std::endl;
+    // PngppImage pngppimage;
+    // if(pngppimage.OpenImage(file_path)){
+    //     uint32_t w = pngppimage.GetW();
+    //     uint32_t h = pngppimage.GetH();
+    //     pngppimage.ShowImageInfo(w,h);
+    // }
 
     return 0;
 }
